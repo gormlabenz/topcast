7ed6eb96b50392eb70f28212501b5d1a69b283cc
gormlabenz <gorm@labenz.io>
2023-04-21 17:16:03 +0200

    added wikipedia route

--- server/__init__.py
+++ server/__init__.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Optional
-import os
 import asyncio
+from wikipediaapi import Wikipedia
 
 from .podcast_chunk import PodcastChunk
 from .configs import summary_config, interview_config, conclusion_config
@@ -11,11 +11,28 @@ from .podcast_builder import PodcastBuilder
 sound_folder = "sounds/"
 output_file="interview.wav"
 
+app = FastAPI()
+
 class Item(BaseModel):
     text: str
     tts_provider: Optional[str] = "gcp"
+
+class SearchTerm(BaseModel):
+    term: str
+    tts_provider: Optional[str] = "gcp"
+
+@app.post("/wikipedia/")
+async def generate_podcast_wikipedia(search_term: SearchTerm):
+    wiki = Wikipedia('en')
+    page = wiki.page(search_term.term)
     
-app = FastAPI()
+    if not page.exists():
+        return {"error": f"Page not found for the search term '{search_term.term}'"}
+
+    summary = page.summary[0:1000]  # Get the first 1000 characters of the summary
+
+    item = Item(text=summary, tts_provider=search_term.tts_provider)
+    return await generate_podcast_text(item=item)
 
 @app.post("/generate_podcast/")
 async def generate_podcast_text(item: Item):

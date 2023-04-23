4519f130de8ab19705b34db25117553082323822
gormlabenz <gorm@labenz.io>
2023-04-23 21:04:30 +0200

    extracted api keys

--- generate.py
+++ generate.py
@@ -1,6 +1,11 @@
 from topcast.chatgpt_themes import Introduction, Interview, Conclusion, Summary
 from topcast.tts_providers import ElevenLabs, GCP
-from topcast import generate
+import os
+from topcast import generate, set_elevenlab_api_key, set_google_credentials, set_openai_api_key
+
+set_elevenlab_api_key(os.environ["ELEVENLAB_API_KEY"])
+set_openai_api_key(os.environ["OPENAI_API_KEY"])
+set_google_credentials('gcp-keyfile.json')
 
 timeline = [
     {
@@ -32,31 +37,8 @@ timeline = [
     "crossfade": 2400, # milliseconds, how much time to overlap the audio with the step before
     "fade-out": 1800,
 },
-    {
-        "audio_layers": [
-            {
-                "audio": {
-                "content": "Der Siamesische Kampffisch ist ein in Thailand und Kambodscha beheimateter Labyrinthfisch. Das erste Auftauchen dieser Fischart wird auf 1892 in Japan datiert. Sie wurde aber bereits viele Jahre zuvor von Einheimischen, vor allem wegen der Aggressivität der Männchen untereinander, für Schau- und Wettkämpfe gezüchtet.",
-                "theme": Interview,
-                "tts_provider": GCP
-                }, # audio as a byte array,
-                "is_main": True, # if True, this audio sets the length of the step in the timeline
-                "padding_start": 1600, # milliseconds, how much time to add before the audio gets played
-                "padding_end": 1200, # milliseconds, how much time to add after the audio gets played
-                "volume": 1 # volume of the audio, 1 is normal, 0 is muted
-            },
-            {
-                "audio": "sounds/554127__legnalegna55__aquarium-sound.mp3",
-                "fade-out": 1800,
-            }
-        ],
-        "fade-in": 0, # milliseconds, how much time to fade in the audio
-        "crossfade": 2400, # milliseconds, how much time to overlap the audio with the step before
-        "fade-out": 1800,
-    },     
 ]
 
-
 podcast = generate(timeline)
-podcast.export("podcast.wav", format="wav")
+podcast.export("podcast_2.wav", format="wav")
 
--- topcast/__init__.py
+++ topcast/__init__.py
@@ -5,3 +5,15 @@ def generate(timeline: Timeline):
     podcaster = Podcaster(timeline)
     podcast = podcaster.generate()
     return podcast
+
+def set_elevenlab_api_key(api_key: str):
+    from elevenlabs import set_api_key
+    set_api_key(api_key)
+
+def set_openai_api_key(api_key: str):
+    import openai
+    openai.api_key = api_key
+
+def set_google_credentials(filename: str):
+    import os
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = filename

6fcb5fb23feb5bf988fb9254997dd7aa51cc33b8
gormlabenz <gorm@labenz.io>
2023-04-26 21:17:14 +0200

    added podcast.wav

--- generate.py
+++ generate.py
@@ -1,7 +1,9 @@
-from topcast import Topcaster, set_openai_api_key
-from topcast.chatgpt_themes import Introduction
+from topcast import Topcaster, set_openai_api_key, set_elevenlab_api_key
+from topcast.chatgpt_themes import Summary
+from topcast.tts_providers import  ElevenLabs, GTTS
 
-set_openai_api_key("XXX-XXX-XXX-XXX-XXX")
+set_openai_api_key("7a8f7e9bbc46dcfaa0d5634b1dfeb634")
+set_elevenlab_api_key("7a8f7e9bbc46dcfaa0d5634b1dfeb634")
 
 topcast = Topcaster()
 
@@ -11,12 +13,12 @@ topcast.add_chapter(
         {
             "audio": {
                 "content": "Topcast is a Python package that allows you to transform text into a podcast using Text-to-Speech (TTS) and language models. With Topcast, you can provide a text, and the package will create a podcast with an introduction, interview, conclusion, sound effects, and more. Topcast supports various TTS providers and language models.",
-                "theme": Introduction,
+                "theme": Summary,
+                "tts_provider": ElevenLabs
             },
         },
     ],
-    crossfade=2400,
 )
 
 topcast.generate()
-topcast.export("podcast.wav", format="wav")
+topcast.export("podcast_topcast.wav", format="wav")

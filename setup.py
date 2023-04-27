22ee01ecb9ae0da3fcfaa116c8b6b25abb5f3420
gormlabenz <gorm@labenz.io>
2023-04-27 23:24:01 +0200

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="topcast",
-    version="0.1.6",
+    version="0.1.7",
     author="Gorm Labenz",
     author_email="gorm@labenz.io",
     description="A Python package for Topcast",

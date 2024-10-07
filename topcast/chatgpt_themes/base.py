e510429d22dfa325e94236aec630f1dba1360bbe
gormlabenz <gorm@labenz.io>
2024-10-07 19:41:38 +0200

    updated chatgpt model

--- topcast/chatgpt_themes/base.py
+++ topcast/chatgpt_themes/base.py
@@ -1,32 +1,34 @@
 from abc import ABC, abstractmethod
+
 from openai import OpenAI
 
 client = OpenAI()
+
 
 class ChatGPTThemeBase(ABC):
     def __init__(self):
         self.system_prompt = ""
         self.messages = []
 
-    def get_messages(self, input_message:str):
+    def get_messages(self, input_message: str):
         messages = self.messages.copy()
         # insert system prompt at the beginning
         messages.insert(0, {
-            "role":"system", "content": self.system_prompt})
+            "role": "system", "content": self.system_prompt})
 
         messages.append({
-          "role":"user", "content": self.create_content(input_message)})
+            "role": "user", "content": self.create_content(input_message)})
 
         return messages
 
     async def create_chat_completion(self, input_message: str):
-            messages = self.get_messages(input_message=input_message)
+        messages = self.get_messages(input_message=input_message)
 
-            completion = client.chat.completions.create(model="gpt-3.5-turbo",
-            messages=messages)
-            content = completion.choices[0].message.content
+        completion = client.chat.completions.create(model="gpt-4o",
+                                                    messages=messages)
+        content = completion.choices[0].message.content
 
-            return self.extract_content(content)
+        return self.extract_content(content)
 
     @abstractmethod
     def create_content(self, input_content: str):
